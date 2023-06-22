e5d632bee72d6cc2957c9388188f8e25be3fba72
TylerRLowe <trl59@pitt.edu>
2023-06-21 23:06:52 -0400

    more methods

--- string_builder.py
+++ string_builder.py
@@ -83,3 +83,67 @@ class StringBuilder:
             self.buffer = self.buffer[:new_max_length]
             self.count = new_max_length
         
+    def clear(self):
+        """
+        Clears the buffer by setting it to an empty list.
+        """
+        self.buffer = []
+        self.count = 0
+        
+    def delete(self, start: int, end: int):
+        """
+        Delete characters from this StringBuilder.
+
+        Args:
+            start (int): The first character to delete.
+            end (int): The index after the last character to delete.
+
+        Raises:
+            ValueError: If start or end are out of bounds.
+        """
+        if start < 0 or start > self.count or start > end:
+            raise ValueError("Invalid start index")
+        if end > self.count:
+            end = self.count
+        self.buffer[start:end] = []
+        self.count -= end - start
+        
+    def char_at(self, index: int) -> str:
+        """
+        Returns the character at the specified index.
+
+        Args:
+            index (int): The index of the character.
+
+        Returns:
+            str: The character at the specified index.
+
+        Raises:
+            IndexError: If the index is out of bounds.
+        """
+        if index < 0 or index >= self.count:
+            raise IndexError("Index out of bounds")
+        return self.buffer[index]
+    
+    def string_at(self, start: int, length: int) -> str:
+        """
+        Returns a substring starting from the specified index with the given length.
+
+        Args:
+            start (int): The starting index of the substring.
+            length (int): The length of the substring.
+
+        Returns:
+            str: The substring.
+
+        Raises:
+            ValueError: If the start or length values are invalid.
+        """
+        if start < 0 or start >= self.count:
+            raise ValueError("Invalid start index")
+        if length < 0:
+            raise ValueError("Invalid length")
+        end = start + length
+        if end > self.count:
+            end = self.count
+        return ''.join(self.buffer[start:end])
